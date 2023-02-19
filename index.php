365fb34f145032e96bfef8f5a4ceef7b18d980e9
Ken Ingram <ken.ingram@gmail.com>
2023-02-18 18:18:50 -0800

    Set up containers

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.3/axios.min.js" integrity="sha512-wS6VWtjvRcylhyoArkahZUkzZFeKB7ch/MHukprGSh1XIidNvHG1rxPhyFnL73M0FC1YXPIXLRDAoOyRJNni/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script defer src="js/main.js"></script>
   <title>Document</title>
 </head>
 <body>
-  
+  <header>Cold Storage</header>
+  <div class="main">
+    <div class="secret-container"></div>
+    <div class="client-container"></div>
+  </div>
+  <footer><span class="datetime"></span></footer>
 </body>
 </html>
